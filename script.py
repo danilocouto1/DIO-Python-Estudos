9af366e50d53d08ee38546898d26bea82a4909e7
Danilo Santos <dansantosalfa@gmai.com>
2024-04-19 18:47:41 -0300

    fix:  corrigindo brechas do sistema

--- script.py
+++ script.py
@@ -16,32 +16,33 @@ def sacar(saque: int):
     global conta_global
     if saque > conta_global:
         print("Valor acima do permitido!")
-    else:
+    elif saque > 0:
         conta_global -= saque
         print("Saque efetuado com sucesso.")
+    else:
+        print("Valor invalido!")
 
 
 while section != 0:
-    section = int(
-        input(
-            """
+    section = input(
+        """
         Digite:
         1 - Depositar
         2 - Sacar
         3 - Extrato
         0 - Fechar o programa
         """
-        )
     )
-    if section == 1:
+
+    if int(section) == 1:
         deposito = int(input("Quanto deseja depositar?\n"))
         depositar(deposito)
-    elif section == 2:
+    elif int(section) == 2:
         saque = int(input("Quanto deseja sacar?\n"))
         sacar(saque)
-    elif section == 3:
+    elif int(section) == 3:
         print(f"Valor em conta é: {conta_global}")
-    elif section == 0:
+    elif int(section) == 0:
         print("Progama finalizado! Obrigado por usar serviços DIO.")
     else:
         print("Valor invalido! Digite Novamente.")
